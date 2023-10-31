80ca423f581f31964457a8f87c8ab9c6efe691aa
LAPTOP-3H857UAQ\Pranav <venkynani.maddneni@gmail.com>
2023-10-31 21:34:52 +0530

    first commit

---
+++ Controllers/UserDetailsController.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Sample1.Models;
+
+namespace Sample1.Controllers
+{
+    public class UserDetailsController : Controller
+    {
+        private DevBoxEntities1 db = new DevBoxEntities1();
+
+        // GET: UserDetails
+        public ActionResult Index()
+        {
+            return View(db.User_Details.ToList());
+        }
+
+       /* if(User_Details.ResumePdf!=null){
+            String folder = "Details/";
+        User_Details.ResumeUrl=await UploadFile(folder , User_Details.ResumePdf);
+        }*/
+
+
+        // GET: UserDetails/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User_Details user_Details = db.User_Details.Find(id);
+            if (user_Details == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_Details);
+        }
+
+        // GET: UserDetails/Create
+        public ActionResult Create()
+        {
+
+            YourAction();
+            CitiesDropDown();
+            return View();
+        }
+
+        // POST: UserDetails/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Email,State,City,MobileNumber,Resume,Photo")] User_Details user_Details)
+        {
+            if (ModelState.IsValid)
+            {
+                db.User_Details.Add(user_Details);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user_Details);
+        }
+
+        // GET: UserDetails/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User_Details user_Details = db.User_Details.Find(id);
+            if (user_Details == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_Details);
+        }
+
+        // POST: UserDetails/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Email,State,City,MobileNumber,ResumeUrl,PhotoUrl")] User_Details user_Details)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user_Details).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user_Details);
+        }
+
+        // GET: UserDetails/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User_Details user_Details = db.User_Details.Find(id);
+            if (user_Details == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_Details);
+        }
+
+        // POST: UserDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            User_Details user_Details = db.User_Details.Find(id);
+            db.User_Details.Remove(user_Details);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        public List<State> GetAllStates()
+        {
+            List<State> states = new List<State>();
+            using (var connection = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["DevBox"].ConnectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM States";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        states.Add(new State
+                        {
+                            Id = (int)reader["Id"],
+                            StateName = (string)reader["StateName"]
+                        });
+                    }
+                }
+            }
+            return states;
+        }
+        public ActionResult YourAction()
+        {
+            List<State> states = GetAllStates(); // Replace with your method to get the list of states
+            ViewBag.States = new SelectList(states, "Id", "StateName"); // Assuming 'Id' and 'StateName' are properties of your State object
+            return View("Temp");
+        }
+        public List<City>GetAllCitiesForState(State obj)
+        {
+         
+            string query = "SELECT * FROM cities WHERE StateName = @StateName"; // or "SELECT * FROM City WHERE StateId = @StateId";
+            List<City> cities = new List<City>();
+
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DevBox"].ConnectionString))
+            {
+                connection.Open();
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    // Add parameters to the query based on your chosen identifier (StateName or StateId)
+                    // For StateName:
+                    command.Parameters.Add(new SqlParameter("@StateName", SqlDbType.NVarChar) { Value = obj.StateName });
+                    // For StateId:
+                    // command.Parameters.Add(new SqlParameter("@StateId", SqlDbType.Int) { Value = obj.StateId });
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            cities.Add(new City
+                            {
+                                ID = (int)reader["Id"],
+                                CityName = (string)reader["CityName"]
+                            });
+                        }
+                    }
+                }
+            }
+            return cities;
+
+        }
+        State temporaryState = new State
+            {
+                Id = 2, 
+                StateName = "Telangana"
+         };
+
+
+            public ActionResult CitiesDropDown()
+        {
+            List<City> cities = GetAllCitiesForState(temporaryState); // Replace with your method to get the list of states
+            ViewBag.Cities = new SelectList(cities, "Id", "CityName"); // Assuming 'Id' and 'StateName' are properties of your State object
+            return View("TempCities");
+        }
+
+
+
+
+    }
+}
---
+++ Controllers/UserRegistrationController.cs
@@ -0,0 +1,187 @@
+﻿using MimeKit;
+using Sample1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample1.Controllers
+{
+    public class UserRegistrationController : Controller
+    {
+
+        int count = 1;// GET: UserRegistration
+        DevBoxEntities1 db = new DevBoxEntities1();
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        // GET: UserRegistration/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserRegistration/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserRegistration/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                if (collection != null)
+                {
+                    var model = new UsersRegistration
+                    {
+
+                        
+                        Name = collection["Name"],
+                        MobileNumber=collection["MobileNumber"],
+                        Email=collection["Email"],
+                        
+
+                    };
+                    db.Entry(model).State = EntityState.Added;
+                    db.SaveChanges();
+                    String email = collection["Email"];
+                    Session["Email"] = email;
+                }
+                MailVerification();
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                ViewBag.Message = "An error occurred while adding data User already exsists.";
+                return View();
+            }
+        }
+
+        // GET: UserRegistration/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserRegistration/Edit/5
+       [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserRegistration/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserRegistration/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public String MailVerification()
+        {
+
+
+            /*            string fromMail = "venki.nani2001@gmail.com";
+                        string toMail = "venkynani.maddineni@gmail.com";
+                        string pass = "E.pranav@123"; // Use the app password you generated
+
+                        MailAddress to = new MailAddress(toMail);
+                        MailAddress from = new MailAddress(fromMail);
+
+                        using (MailMessage mail = new MailMessage(from, to))
+                        {
+                            mail.Subject = "Verify your email within 10 mins";
+                            mail.Body = "Verify your email and set your password";
+
+                            using (SmtpClient smtp = new SmtpClient("smtp.gmail.com"))
+                            {
+                                smtp.Port = 587;
+                                smtp.Credentials = new NetworkCredential(fromMail, pass);
+                                smtp.EnableSsl = true;
+
+                                try
+                                {
+                                    smtp.Send(mail);
+                                    return "Email sent successfully.";
+                                }
+                                catch (Exception ex)
+                                {
+                                    return "Email sending failed: " + ex.Message;
+                                }
+                            }
+                        }*/
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress("venki.nani2001@gmail.com");
+                mail.To.Add("vasudev.maddineni@domain.com");
+                mail.Subject = "Hello World";
+                mail.Body = "<h1>Hello</h1>";
+                mail.IsBodyHtml = true;
+               // mail.Attachments.Add(new Attachment("C:\\file.zip"));
+
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtp.Credentials = new NetworkCredential("venkinani.maddineni@gmail.com", "E.pranav@123");
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                }
+                return "chance";
+            }
+
+
+            /*            var mailMessage = new MimeMessage();
+                        mailMessage.From.Add(new MailboxAddress("from name", "from email"));
+                        mailMessage.To.Add(new MailboxAddress("to name", "to email"));
+                        mailMessage.Subject = "subject";
+                        mailMessage.Body = new TextPart("plain")
+                        {
+                            Text = "Hello"
+                        };
+
+                        using (var smtpClient = new SmtpClient())
+                        {
+                            smtpClient.Connect("smtp.gmail.com", 587, true);
+                            smtpClient.Authenticate("user", "password");
+                            smtpClient.Send(mailMessage);
+                            smtpClient.Disconnect(true);
+                            return "Every thing is Well";
+                        }*/
+
+        }
+    }
+}
